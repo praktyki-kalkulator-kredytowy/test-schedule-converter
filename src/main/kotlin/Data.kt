cb17812fb8ee6f107cc749c9ea51d483ec037d42
xsw2-2wsx <jan1undrych@gmail.com>
2021-05-18 17:10:00 +0200

    feat(converter): add convertion to new data

--- src/main/kotlin/Data.kt
+++ src/main/kotlin/Data.kt
@@ -3,22 +3,46 @@ import java.math.BigDecimal
 import java.time.LocalDate
 
 
-data class TestCase(
-    var configuration: ScheduleConfiguration,
-    var installments: List<Installment>
+data class ScheduleTestCase(
+    @field:JsonProperty("mTestScheduleConfiguration")
+    var testScheduleConfiguration: TestScheduleConfiguration,
+    @field:JsonProperty("mInstallmentList")
+    var installmentList: List<Installment>,
+    @field:JsonProperty("mInsurancePremiumList")
+    var insurancePremiumList: List<InsurancePremium>,
+    @field:JsonProperty("mSumUpCapitalInstallment")
+    var sumUpCapitalInstallment: BigDecimal,
+    @field:JsonProperty("mLoanPaidOutAmount")
+    var loanPaidOutAmount: BigDecimal,
+    @field:JsonProperty("mCommissionAmount")
+    var commissionAmount: BigDecimal,
+    @field:JsonProperty("mInsuranceTotalAmount")
+    var insuranceTotalAmount: BigDecimal,
+    @field:JsonProperty("mLoanTotalCost")
+    var loanTotalCost: BigDecimal,
+    @field:JsonProperty("mAPRC")
+    var aprc: BigDecimal,
 )
 
-data class ScheduleConfiguration(
+data class TestScheduleConfiguration(
     @field:JsonProperty("mCapital")
     var capital: BigDecimal,
-    @field:JsonProperty("mInstallmentAmount")
-    var installmentAmount: Int,
     @field:JsonProperty("mInstallmentType")
     var installmentType: InstallmentType,
+    @field:JsonProperty("mInstallmentAmount")
+    var installmentAmount: Int,
     @field:JsonProperty("mInterestRate")
     var interestRate: Double,
     @field:JsonProperty("mWithdrawalDate")
     var withdrawalDate: LocalDate,
+    @field:JsonProperty("mCommissionRate")
+    var commissionRate: Double,
+    @field:JsonProperty("mInsurance")
+    var insurance: Boolean,
+    @field:JsonProperty("mAge")
+    var age: Int,
+    @field:JsonProperty("mMonthFrame")
+    var monthFrame: Int
 )
 
 data class Installment(
@@ -32,6 +56,15 @@ data class Installment(
     var interestInstallment: BigDecimal,
     @field:JsonProperty("mRemainingDebt")
     var remainingDebt: BigDecimal,
+)
+
+data class InsurancePremium(
+    @field:JsonProperty("mIndex")
+    var index: Int,
+    @field:JsonProperty("mInsurancePremiumDate")
+    var insurancePremiumDate: LocalDate,
+    @field:JsonProperty("mInsurancePremiumValue")
+    var insurancePremiumValue: BigDecimal,
 )
 
 enum class InstallmentType {
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,7 @@
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import java.io.File
-import java.io.PrintStream
+import java.math.RoundingMode
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.util.*
@@ -14,7 +14,7 @@ val mapper = ObjectMapper().registerModule(JavaTimeModule())
 
 fun main() {
 
-    val testCases = LinkedList<TestCase>()
+    val testCases = LinkedList<ScheduleTestCase>()
 
     val lineIterator = File("src/main/resources/source.txt").readLines().filter { it.isNotBlank() }.iterator()
 
@@ -25,32 +25,70 @@ fun main() {
 
 }
 
-fun readCase(lineIterator: Iterator<String>): TestCase {
+fun readCase(lineIterator: Iterator<String>): ScheduleTestCase {
 
-    val conf = ScheduleConfiguration(
+    val conf = TestScheduleConfiguration(
         capital = lineIterator.next().fixNumFormat().toBigDecimal(),
         installmentAmount = lineIterator.next().toInt(),
         installmentType = InstallmentType.valueOf(lineIterator.next()),
         interestRate = lineIterator.next().toDouble() / 100,
-        withdrawalDate = LocalDate.parse(lineIterator.next(), DateTimeFormatter.ofPattern("M/dd/yyyy")),
+        withdrawalDate = LocalDate.parse(lineIterator.next(), DateTimeFormatter.ofPattern("M/d/yyyy")),
+        age = lineIterator.next().toInt(),
+        commissionRate = lineIterator.skip(1).next().toDouble() / 100,
+        monthFrame = lineIterator.next().toInt(),
+        insurance = true,
     )
 
+    val aprc = lineIterator.next().fixNumFormat().toBigDecimal() / 100.toBigDecimal()
+
     val installments = LinkedList<Installment>()
+    val insurancePremiums = LinkedList<InsurancePremium>()
 
     repeat(conf.installmentAmount) {
-        val data = lineIterator.next().split("""\s""".toRegex()).toMutableList().apply { skipList.forEach { removeAt(it) } }
+        val data = lineIterator
+            .next()
+            .split("""\s""".toRegex())
+            .toMutableList()
+            .apply { skipList.forEach { removeAt(it) } }
+            .iterator()
 
         installments.add(Installment(
-            data.removeAt(0).fixNumFormat().toInt(),
-            LocalDate.parse(data.removeAt(0), DateTimeFormatter.ofPattern("M/dd/yyyy")),
-            data.removeAt(0).fixNumFormat().toBigDecimal(),
-            data.removeAt(0).fixNumFormat().toBigDecimal(),
-            data.removeAt(0).fixNumFormat().toBigDecimal(),
+            index = data.next().fixNumFormat().toInt(),
+            installmentDate = LocalDate.parse(data.next(), DateTimeFormatter.ofPattern("M/d/yyyy")),
+            capitalInstallment = data.next().fixNumFormat().bd(),
+            interestInstallment = data.next().fixNumFormat().bd(),
+            remainingDebt = data.next().fixNumFormat().bd(),
         ))
+        
+        val insuranceValue = data.next().bd()
 
+        if(insuranceValue > 0.toBigDecimal()) insurancePremiums.add(InsurancePremium(
+            index = insurancePremiums.size + 1,
+            insurancePremiumDate = installments.last.installmentDate,
+            insuranceValue
+        ))
     }
 
-    return TestCase(conf, installments)
+    val summary = lineIterator
+        .next()
+        .split("""\s""".toRegex())
+        .iterator()
+
+    return ScheduleTestCase(
+        testScheduleConfiguration = conf,
+        installmentList = installments,
+        insurancePremiumList = insurancePremiums,
+        loanPaidOutAmount = summary.next().fixNumFormat().bd(),
+        commissionAmount = summary.next().fixNumFormat().bd(),
+        sumUpCapitalInstallment = summary.next().fixNumFormat().bd(),
+        loanTotalCost = summary.skip(2).next().fixNumFormat().bd(),
+        insuranceTotalAmount = summary.next().fixNumFormat().bd(),
+        aprc = aprc,
+    )
 }
 
 fun String.fixNumFormat() = this.replace(",", "")
+
+fun <T> Iterator<T>.skip(amount: Int): Iterator<T> = apply { repeat(amount) { next() } }
+
+fun String.bd() = toBigDecimal().setScale(2, RoundingMode.HALF_UP)
